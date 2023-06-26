6771f3c501e22c1795b7160982f538a764fcff7d
favour2333 <kingsleyfavour999@gmail.com>
2023-06-26 09:27:37 -0700

    initial

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -10,4 +10,6 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
     [`capita-${getCurrentYear()}`]: capita,
   };
 }
+console.log(getBudgetForCurrentYear('$119,868', '$154.2 billion', '$178,479'));
+// Output: { "income-2023": "$119,868", "gdp-2023": "$154.2 billion", "capita-2023": "$178,479" }
 
