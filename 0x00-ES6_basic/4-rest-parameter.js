0728d416b573c07cbff1ecde58a010aa2e3f0787
favour2333 <kingsleyfavour999@gmail.com>
2023-06-26 08:59:37 -0700

    initial

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -3,5 +3,3 @@ export default function returnHowManyArguments(...args) {
 }
 console.log(returnHowManyArguments("Hello", "Holberton", 2020)); // Output: 3
 console.log(returnHowManyArguments(1, 2, 3, 4, 5)); // Output: 5
-console.log(returnHowManyArguments()); // Output: 0 (no arguments passed)
-
