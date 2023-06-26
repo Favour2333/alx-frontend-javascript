08aa3eb7d1649130184bf86b849c895e8bf3bae6
favour2333 <kingsleyfavour999@gmail.com>
2023-06-26 09:03:44 -0700

    initial

---
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -0,0 +1,9 @@
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
+}
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const string = "Hello";
+console.log(concatArrays(array1, array2, string));
+// Output: [1, 2, 3, 4, 5, 6, "H", "e", "l", "l", "o"]
+
