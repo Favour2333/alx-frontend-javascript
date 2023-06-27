706f725a0f9e475e713bd1990087713ddbd9c65c
favour2333 <kingsleyfavour999@gmail.com>
2023-06-27 09:14:07 -0700

    initial

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,4 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Perform API request or any asynchronous operation here
-    // For demonstration purposes, we'll resolve the promise after a delay of 1 second
-    setTimeout(() => {
-      resolve("API response");
-    }, 1000);
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
-import getResponseFromAPI from "./0-promise.js";
-
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);
 
