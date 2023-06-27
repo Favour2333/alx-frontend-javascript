3728a00c6a3d3f4c9b98f85a8a4f4c68cea9e7c3
favour2333 <kingsleyfavour999@gmail.com>
2023-06-27 07:11:26 -0700

    initial

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,20 +1,13 @@
 function getResponseFromAPI() {
   return new Promise((resolve) => {
-    // Simulating an API request
-    setTimeout(() => {
-      const response = {
-        status: 200,
-        body: 'photo-profile-1',
-      };
-
-      resolve(response);
-    }, 2000);
+    const response = {
+      status: 200,
+      body: 'photo-profile-1',
+    };
+    
+    resolve(response);
   });
 }
 
 export default getResponseFromAPI;
-import getResponseFromAPI from "./0-promise.js";
-
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);
 
