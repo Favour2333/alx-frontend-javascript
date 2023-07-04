a5a6bb237c1bca2afc8f0f6265a8cc92dff7dd30
favour2333 <kingsleyfavour999@gmail.com>
2023-07-04 04:39:33 -0700

    initial

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,14 @@
+/**
+ * This function retrieves an array of student ids from a list of objects.
+ * Author: Kingsley Favour <https://github.com/favour233>
+ * @param {Array} students - The list of student objects.
+ * @returns {Array} - An array of student ids.
+ */
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  return students.map(student => student.id);
+}
+
