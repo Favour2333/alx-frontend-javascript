024598d619b9314fe22978cce6d289d407bfc8ff
favour2333 <kingsleyfavour999@gmail.com>
2023-07-04 04:20:33 -0700

    initial

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,15 @@
-function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-  ];
+/**
+ * This function retrieves ids from a list of students
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String,
+ * }[]} students - The list of students.
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
 }
-
